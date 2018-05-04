e37253541e5f65efdac44a49649d150fbff765c7
Sebastian Kaim <sebb@sebb767.de>
2018-05-04 23:16:19 +0200

    Fixed rc.sh hangs and multiple startups

--- awesome/rc.sh
+++ awesome/rc.sh
@@ -9,16 +9,16 @@ pulseaudio --check || pulseaudio --daemonize --start &
 if [ -s ~/.Xmodmap ]; then
     xmodmap ~/.Xmodmap
 fi
-pgrep compton || compton -cCf -I 1 -O 0.04 &
+killall compton; compton -cCf -I 1 -O 0.04 &
 
 #
 # demons & applets
 #
-pgrep redshift || redshift -l 49.75:11.07 &
-pgrep mate-screensaver || mate-screensaver & # auto singleton, but throws unnice errors
+killall redshift; redshift -l 49.75:11.07 &
+killall mate-screensaver; mate-screensaver & # auto singleton, but crashes on x restart
 # lock screen after some time
 [ -z "$1" ] && grep xautolock || xautolock -time 5 -notifier "echo 'lock_screen_timeout()' | awesome-client" -notify 10 -locker "/home/sebb/bin/lock" &
-[ -z "$1" ] && pgrep nm-applet || $HOME/bin/nm-applet-loop.sh & #
+[ -z "$1" ] && ( pgrep nm-applet || $HOME/bin/nm-applet-loop.sh & ) #
 
 #
 # applications
